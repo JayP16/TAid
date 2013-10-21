1429717c9fe061db60af511f7f3ce2745a338f07
JayP16 <jayr.patel@mail.utoronto.ca>
2013-10-20 18:52:11 -0700

    Successfully registers users to the database and adds to server.

--- TAid/gen/com/example/taid/R.java
+++ TAid/gen/com/example/taid/R.java
@@ -35,30 +35,56 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     public static final class drawable {
         public static final int bk_red=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int splash_background=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f090005;
+        public static final int action_forgot_password=0x7f09000d;
+        public static final int action_settings=0x7f09000c;
+        public static final int email=0x7f090009;
         public static final int fullscreen_content=0x7f090000;
         public static final int fullscreen_content_controls=0x7f090001;
+        public static final int login=0x7f09000a;
+        public static final int login_form=0x7f090008;
+        public static final int login_status=0x7f090006;
+        public static final int login_status_message=0x7f090007;
         public static final int password=0x7f090003;
+        public static final int register=0x7f090005;
+        public static final int register_button=0x7f09000b;
         public static final int submit=0x7f090004;
         public static final int user_id=0x7f090002;
     }
     public static final class layout {
         public static final int activity_display_message=0x7f030000;
         public static final int activity_fullscreen=0x7f030001;
+        public static final int activity_register=0x7f030002;
+        public static final int splash=0x7f030003;
     }
     public static final class menu {
         public static final int display_message=0x7f080000;
+        public static final int register=0x7f080001;
     }
     public static final class string {
+        public static final int action_forgot_password=0x7f06000e;
         public static final int action_settings=0x7f060004;
+        public static final int action_sign_in_register=0x7f06000c;
+        public static final int action_sign_in_short=0x7f06000d;
         public static final int app_name=0x7f060000;
         public static final int dummy_content=0x7f060002;
+        public static final int error_field_required=0x7f060013;
+        public static final int error_incorrect_password=0x7f060012;
+        public static final int error_invalid_email=0x7f060010;
+        public static final int error_invalid_password=0x7f060011;
         public static final int hello_world=0x7f060005;
         public static final int log_in=0x7f060006;
-        public static final int pass=0x7f060007;
+        public static final int login_progress_signing_in=0x7f06000f;
+        public static final int pass=0x7f060008;
+        /**  Strings related to login 
+         */
+        public static final int prompt_email=0x7f06000a;
+        public static final int prompt_password=0x7f06000b;
+        public static final int reg=0x7f060007;
         public static final int title_activity_display_message=0x7f060003;
+        public static final int title_activity_register=0x7f060009;
         public static final int user_id=0x7f060001;
     }
     public static final class style {
@@ -66,11 +92,13 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
 
+
     
 
             Theme customizations available in newer API levels can go in
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
+
 
         
 
@@ -93,8 +121,9 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int AppTheme=0x7f070001;
         public static final int ButtonBar=0x7f070003;
         public static final int ButtonBarButton=0x7f070004;
-        public static final int FullscreenActionBarStyle=0x7f070006;
+        public static final int FullscreenActionBarStyle=0x7f070007;
         public static final int FullscreenTheme=0x7f070002;
+        public static final int LoginFormContainer=0x7f070006;
         public static final int sa=0x7f070005;
     }
     public static final class styleable {
--- TAid/src/com/example/taid/FullscreenActivity.java
+++ TAid/src/com/example/taid/FullscreenActivity.java
@@ -33,24 +33,14 @@ public class FullscreenActivity extends Activity {
     private EditText userName;
     private EditText passw;
     private Button btnSubmit;
+	private Exception exception;
+    private Button btnReg;
  
-    /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
 	public final static String userId = "com.example.TAid.MESSAGE";
 	public final static String password = "com.example.TAid.MESSAGE";
 	
-
-
-    /**
-     * The flags to pass to {@link SystemUiHider#getInstance}.
-     */
     private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION;
 
-    /**
-     * The instance of the {@link SystemUiHider} for this activity.
-     */
     private SystemUiHider mSystemUiHider;
 
     @Override
@@ -76,10 +66,6 @@ public class FullscreenActivity extends Activity {
                     @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
                     public void onVisibilityChange(boolean visible) {
                         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-                            // If the ViewPropertyAnimator API is available
-                            // (Honeycomb MR2 and later), use it to animate the
-                            // in-layout UI controls at the bottom of the
-                            // screen.
                             if (mControlsHeight == 0) {
                                 mControlsHeight = controlsView.getHeight();
                             }
@@ -91,20 +77,13 @@ public class FullscreenActivity extends Activity {
                                     .translationY(visible ? 0 : mControlsHeight)
                                     .setDuration(mShortAnimTime);
                         } else {
-                            // If the ViewPropertyAnimator APIs aren't
-                            // available, simply show or hide the in-layout UI
-                            // controls.
                             controlsView.setVisibility(visible ? View.VISIBLE : View.GONE);
                         }
 
                     }
                 });
 
-        
-
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
+    
         findViewById(R.id.submit).setOnTouchListener(mDelayHideTouchListener);
         registerViews();
     }
@@ -140,7 +119,6 @@ public class FullscreenActivity extends Activity {
             mSystemUiHider.hide();
         }
     };
-	private Exception exception;
 
  private void registerViews() {
  	userName = (EditText) findViewById(R.id.user_id);
@@ -172,9 +150,17 @@ public class FullscreenActivity extends Activity {
              Validation class will check the error and display the error on respective fields
              but it won't resist the form submission, so we need to check again before submit
               */
+        	 
 			if ( checkValidation ())
             	 
             	 submitForm(view);
+         }
+     });
+     btnReg = (Button) findViewById(R.id.register);
+     btnReg.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View view) {            	 
+            	 Register(view);            	 
          }
      });
  }
@@ -182,9 +168,8 @@ public class FullscreenActivity extends Activity {
  private void submitForm(View view) {
 	 String re = CheckAccount();
 	 if (re.equals("1")){
-		 
-		  // Submit your form here. your form is valid
-		 Toast.makeText(this, "Verifying...", Toast.LENGTH_LONG).show();
+		  // Submit form here. form is valid
+		 	Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show();
 			Intent intent = new Intent(this, DisplayMessageActivity.class);
 		    EditText editText = (EditText) findViewById(R.id.user_id);
 		    EditText pass = (EditText) findViewById(R.id.password);
@@ -195,10 +180,14 @@ public class FullscreenActivity extends Activity {
 		    startActivity(intent);
 		 
 	 }
+	 else{
+		 Toast.makeText(this, "Invalid Login. Try Again.", Toast.LENGTH_SHORT).show();
+	 }
    
  }
 
 private boolean checkValidation() {
+	 Toast.makeText(this, "Verifying...", Toast.LENGTH_SHORT).show();
      boolean ret = true;
      if (!Validation.hasText(userName)) ret = false;
      if (!Validation.hasText(passw)) ret = false;
@@ -218,20 +207,25 @@ private String CheckAccount() {
 	    String passwords = pass.getText().toString();
      Socket clientSocket;
      try {
-     	clientSocket = new Socket("192.168.1.70", 6889);
-			BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-			PrintWriter printwriter = new PrintWriter(clientSocket.getOutputStream(),true);
-			printwriter.println("login");
-			printwriter.println(username);
-			printwriter.println(passwords);
-			result = in.readLine();
-			clientSocket.close();
-			in.close();
-			printwriter.close();
+     	clientSocket = new Socket("173.206.206.238", 6889);
+		BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		PrintWriter printwriter = new PrintWriter(clientSocket.getOutputStream(),true);
+		printwriter.println("login");
+		printwriter.println(username);
+		printwriter.println(passwords);
+		result = in.readLine();
+		clientSocket.close();
+		in.close();
+		printwriter.close();
      } catch (Exception e) {
          this.exception = e;
      }
 	return result;
+ }
+ 
+ private void Register(View view) {
+			Intent intent = new Intent(this, RegisterActivity.class);
+		    startActivity(intent);   
  }
  
 }
